2e1e239913a195eea557a7a62a8f2f9710e6cac4
EmilienD <emilien.dieryck@student.vinci.be>
2024-09-17 20:17:10 +0200

    1.1-1.2 : import image

--- Semaine1/exercices/module1/1.1-1.2/1.1/src/components/Header/Header.jsx
+++ Semaine1/exercices/module1/1.1-1.2/1.1/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import image from "./images/LOGO HE VINCI.png";
 
 const Header = (props) => {
     return(
       <>
+        <img src={image} alt="image HE VINCI"></img>
         <h1>{props.course}</h1>
       </>
     )
