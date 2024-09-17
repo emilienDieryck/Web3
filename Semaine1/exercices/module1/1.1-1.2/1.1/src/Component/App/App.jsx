00256cdeaa01942649acbe3961569e39799cf96f
EmilienD <emilien.dieryck@student.vinci.be>
2024-09-17 19:12:58 +0200

    1.1-1.2 : refactor - modules

--- Semaine1/exercices/module1/1.1-1.2/1.1/src/Component/App/App.jsx
+++ Semaine1/exercices/module1/1.1-1.2/1.1/src/Component/App/App.jsx
@@ -1,34 +1,6 @@
-const Header = (props) => {
-  return(
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return(
-    <>
-      <Part name={props.part1} number={props.exercises1} />
-      <Part name={props.part2} number={props.exercises2} />
-      <Part name={props.part3} number={props.exercises3} />
-    </>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <p>
-      {props.name} {props.number}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
+import Header from "../Header/Header"
+import Content from "../Content/Content"
+import Total from "../Total/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
