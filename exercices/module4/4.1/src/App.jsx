cb002ba650f1f0fd91b05113ea59f62d025e4cd7
EmilienD <emilien.dieryck@student.vinci.be>
2024-12-26 15:56:44 +0100

    new:ex2.9

--- exercices/module4/4.1/src/App.jsx
+++ exercices/module4/4.1/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -27,9 +35,17 @@ const App = () => {
     }
   };
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,13 +59,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index ) => (
-          <li key={index}>{person.name} {person.number}</li>
+        {filteredPersons.map(person => (
+          <li key={person.id}>{person.name} {person.number}</li>
         ))}
       </ul>
       <div>debug: {newName} {newNumber}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
