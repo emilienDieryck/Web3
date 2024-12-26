022568da1ca26de2b3ff0eb84c6c7bccb75bc0d5
EmilienD <emilien.dieryck@student.vinci.be>
2024-12-26 16:18:51 +0100

    new:ex2.12

--- exercices/module4/4.1/src/App.jsx
+++ exercices/module4/4.1/src/App.jsx
@@ -36,9 +36,14 @@ const App = () => {
     if (nameExists) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName('');
-      setNewNumber('');
+      const newPerson = { name: newName, number: newNumber };
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        });
     }
   };
 
