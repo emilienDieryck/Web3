28430d24ee05e3d5af66a488acfb9add07367213
EmilienD <emilien.dieryck@student.vinci.be>
2024-12-26 16:02:43 +0100

    new:ex2.10

--- exercices/module4/4.1/src/App.jsx
+++ exercices/module4/4.1/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import Filter from 'components/Filter';
+import PersonForm from 'components/PersonFom';
+import Persons from 'components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -42,28 +45,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={searchTerm} onChange={handleSearchChange} />
-      </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {filteredPersons.map(person => (
-          <li key={person.id}>{person.name} {person.number}</li>
-        ))}
-      </ul>
-      <div>debug: {newName} {newNumber}</div>
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
